c98e1e3909296983b043e3ce89c93c85f8ee2f90
Alejandro Sanabria <alesanabriav@gmail.com>
2015-07-10 18:08:54 -0500

    add prefix css

--- deployment_steps.md
+++
@@ -1,6 +0,0 @@
-## Deployment
-
-- Create repo
-- config express route like [POST] /deploy
-- add sh file with options
-- add hook to url config
--- public/tasks/concact_css.js
+++ public/tasks/concact_css.js
@@ -2,12 +2,17 @@ var gulp      = require('gulp');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var concat    = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('concact_css', function(){
   gulp.src([
     'css/*.scss'
   ])
   .pipe(sass())
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
   .pipe(concat('app.css'))
   .on('error', function(err) {
     console.log(err.toString());
