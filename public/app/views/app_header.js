46a6eccb5f0cc369f9d765e052ae1a0976ea59bb
Alejandro Sanabria <alesanabriav@gmail.com>
2015-07-13 18:46:13 -0500

    fix app header problem with events

--- public/app/views/app_header.js
+++ public/app/views/app_header.js
@@ -12,29 +12,40 @@ module.exports = Backbone.View.extend({
     'click .rotate-button': 'rotate'
   },
 
-  /** Start listen events */
+  //Listen events
   initialize: function() {
     this.listenTo(pubsub, 'view:remove', this.remove, this);
     this.listenTo(pubsub, 'appHeader:change', this.render, this);
-    this.listenTo(pubsub, 'appHeader:showNext', this.showElement('.next-button'), this);
-    this.listenTo(pubsub, 'appHeader:hideNext', this.hideElement('.next-button'), this);
-    this.listenTo(pubsub, 'appHeader:showCheck', this.showElement('.check-button'), this);
-    this.listenTo(pubsub, 'appHeader:hideCheck', this.hideElement('.check-button'), this);
-    this.listenTo(pubsub, 'appHeader:showRotate', this.showElement('.rotate-button'), this);
-    this.listenTo(pubsub, 'appHeader:hideRotate', this.hideElement('.rotate-button'), this);
-    this.listenTo(pubsub, 'appHeader:showBack', this.showElement('.back-button'), this);
-    this.listenTo(pubsub, 'appHeader:hideBack', this.hideElement('.back-button'), this);
+    // this.listenTo(pubsub, 'input:onFocus', this.hide, this);
+    // this.listenTo(pubsub, 'input:onFocusOut', this.show, this);
+
+    this.listenTo(pubsub, 'appHeader:showNext', this.showNext, this);
+    this.listenTo(pubsub, 'appHeader:hideNext', this.hideNext, this);
+
+    this.listenTo(pubsub, 'appHeader:showCheck', this.showCheck, this);
+    this.listenTo(pubsub, 'appHeader:hideCheck', this.hideCheck, this);
+
+    this.listenTo(pubsub, 'appHeader:showRotate', this.showRotate, this);
+    this.listenTo(pubsub, 'appHeader:hideRotate', this.hideRotate, this);
+
+    this.listenTo(pubsub, 'appHeader:showBack', this.showBack, this);
+    this.listenTo(pubsub, 'appHeader:hideBack', this.hideBack, this);
+
     this.listenTo(pubsub, 'appHeader:showClose', this.showClose, this);
     this.listenTo(pubsub, 'appHeader:hideClose', this.hideClose, this);
   },
 
-  /**
-   * attach template with data
-   * @param {object} data
-   * @return {object} this
-   */
+  hide: function() {
+    this.$el.parent().hide();
+  },
+
+  show: function() {
+    this.$el.parent().show();
+  },
+
   render: function(data) {
     var template;
+
     if (data) {
       template = templateHeader(data);
     } else {
@@ -44,35 +55,43 @@ module.exports = Backbone.View.extend({
     $(this.el)
     .empty()
     .append(template);
-
     return this;
   },
 
-  show: function() {
-    this.$el.parent().show();
+  showNext: function() {
+    this.$el.find('.next-button').removeClass('hidden');
   },
 
-  hide: function() {
-    this.$el.parent().hide();
+  showCheck: function() {
+    this.$el.find('.check-button').removeClass('hidden');
   },
 
-  /** show element hidden */
-  showElement: function(el) {
-    this.$el.find(el).removeClass('hidden');
+  showClose: function() {
+    this.$el.find('.close-button').removeClass('hidden');
   },
 
-  /** hide element showed */
-  hideElement: function(el) {
-    this.$el.find(el).removeClass('hidden');
+  showRotate: function() {
+    this.$el.find('.rotate-button').removeClass('hidden');
   },
 
-  /** Trigger event next */
+  backClose: function() {
+    this.$el.find('.back-button').removeClass('hidden');
+  },
+
+  hideCheck: function() {
+    this.$el.find('.check-button').removeClass('hidden');
+  },
+
+  hideNext: function() {
+    this.$el.find('.next-button').addClass('hidden');
+  },
+
+  //execute event
   next: function(e) {
     e.preventDefault();
     pubsub.trigger('app:next');
   },
 
-  /** Trigger event rotate */
   rotate: function(e) {
     e.preventDefault();
     pubsub.trigger('cropper:rotate');
