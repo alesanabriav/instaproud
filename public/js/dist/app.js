a733fad1c3cf6bb8df25c5ac0634d2e0c404f56a
Alejandro Sanabria <alesanabriav@gmail.com>
2015-07-19 13:30:54 -0500

    change url

--- public/js/dist/app.js
+++ public/js/dist/app.js
@@ -54,8 +54,8 @@ window.onload = loadImages();
 'use stricts';
 
 module.exports = {
-  // baseUrl: 'http://instaproud.brandspa.cc',
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'http://instaproud.brandspa.cc',
+  // baseUrl: 'http://localhost:3000',
   s3Bucket: 'https://s3-sa-east-1.amazonaws.com/instaproud'
 };
 
