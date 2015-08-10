4bae6e615c90e4267fc6de6a36536e52000833ff
Alejandro Sanabria <alesanabriav@gmail.com>
2015-08-10 10:21:03 -0500

    change mai

--- mails/verification.js
+++ mails/verification.js
@@ -9,7 +9,7 @@ module.exports = function mailVerification(user, next) {
   email.setSubject('Instaproud confirmar correo');
   email.setHtml('Por favor confirme su correo haciendo click en el link de abajo');
   email.setFrom('noresponder@bvc.com.co');
-  email.addSubstitution('-link-', 'http://instaproud.bvc.com.co/users/' + user.id + '/validation/?code=' + user.id);
+  email.addSubstitution('-linkHref-', 'http://instaproud.bvc.com.co/users/' + user.id + '/validation/?code=' + user.id);
   email.addSubstitution('-linkText-', 'Confirmar Tu correo');
   email.addFilter('templates', 'template_id', 'b6c2a5e3-27e4-425f-90c7-5e0e3c397bec');
   sendgrid.send(email, function(err, json){
